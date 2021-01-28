69e608cd4593696e9aecf28482bede5082f3887c
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2021-01-28 18:16:11 -0500

    Updated GUI to show hero minimap icons in web-GUI

--- gui/web/static/js/index.js
+++ gui/web/static/js/index.js
@@ -1,6 +1,19 @@
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 
+let imageLoaded = false
+let heroData
+// Fetch hero json file
+; (async () => {
+  const response = await fetch('/static/resources/by-name.json')
+  heroData = await response.json()
+})()
+
+// Load the image
+const img = new Image()
+img.onload = () => { imageLoaded = true }
+img.src = '/static/resources/minimap_hero_sheet.png'
+
 const ws = new ReconnectingWebSocket(`ws://${location.host}/ws`)
 
 ws.onmessage = (msg) => {
@@ -8,7 +21,15 @@ ws.onmessage = (msg) => {
   const json = JSON.parse(data)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   for (const entry of json) {
-    const { point: { x, y }, radius, red, blue, green } = entry
+    const { name, isHero, point: { x, y }, radius, red, blue, green } = entry
+    if (isHero && imageLoaded && heroData) {
+      const { [name]: data } = heroData
+      if (data) {
+        const { x: sx, y: sy } = data
+        ctx.drawImage(img, sx, sy, 32, 32, x, y, 32, 32)
+        continue
+      }
+    }
     ctx.beginPath()
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
     ctx.fillStyle = rgbToHex(red, green, blue)
